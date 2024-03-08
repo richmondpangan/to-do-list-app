267142ea650fb83db08d2423bf74f40b020cb6d9
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-03-08 16:05:47 +0800

    Modified ToDoList.jsx
    - Added moveTaskUp and moveTaskDown functions to be able to arrange the
      order of the tasks

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -30,6 +30,22 @@ function TodoList() {
         }
     }
 
+    function moveTaskUp(index) {
+        if (index > 0) {
+            const udpatedTasks = [...tasks];
+            [udpatedTasks[index], udpatedTasks[index - 1]] = [udpatedTasks[index - 1], udpatedTasks[index]];
+            setTasks(udpatedTasks);
+        }
+    }
+
+    function moveTaskDown(index) {
+        if (index < tasks.length - 1) {
+            const udpatedTasks = [...tasks];
+            [udpatedTasks[index], udpatedTasks[index + 1]] = [udpatedTasks[index + 1], udpatedTasks[index]];
+            setTasks(udpatedTasks);
+        }
+    }
+
     return(
         <div className='container'>
             <div className='to-do-list'>
@@ -43,8 +59,14 @@ function TodoList() {
                 <ul>
                     {tasks.map((task, index) => <li key={index} onClick={handleItemClick}>
                         <span className='text'>{task}</span>
+                        <button className='move-down-button' onClick={() => moveTaskDown(index)}>
+                            <img src="./src/assets/down-icon.png" alt="Move down" className='move-down-button-img' />
+                        </button>
+                        <button className='move-up-button' onClick={() => moveTaskUp(index)}>
+                            <img src="./src/assets/up-icon.png" alt="Move up" className='move-up-button-img' />
+                        </button>
                         <button className='delete-button' onClick={() => deleteTask(index)}>
-                            <img src="./src/assets/delete-button.png" alt="x" className='delete-button-img' />
+                            <img src="./src/assets/delete-button.png" alt="Delete" className='delete-button-img' />
                         </button>
                     </li>)}
                 </ul>
